c765349604147bf5cf9bb84a0cf32a408e32dd8c
Ivan Nikolaev <nikolaev_ia@nlmk.com>
2023-01-25 22:40:28 +0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, {createElement as e, useEffect, useState} from 'react';
+
+// import { products } from "./data/products"
+
+import { Product } from "./components/Product"
+import { Loader } from "./components/Loader"
+import { ErrorMessage } from "./components/ErrorMessage"
+import { Modal } from "./components/Modal"
+import { CreateProduct } from "./components/CreateProduct"
+// import { IProduct } from "./models"
+// import axios, { AxiosError } from "axios"
+
+import { useProducts } from "./hooks/products"
 
 function App() {
+  const { loading, products, error } = useProducts()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto max-w-2xl pt-5'>
+      { loading && <Loader /> }
+      { error && <ErrorMessage error={ error } /> }
+      { products.map(product => <Product product={product} key={ product.id } /> ) }
+
+      {/* <Product product={ products[0] } /> */}
+      {/* <Product product={ products[1] } /> */}
+
+      <Modal title="Create new product">
+        <CreateProduct />
+      </Modal>
     </div>
-  );
+  )
+  // return (
+  //   <h1>Hello React</h1>
+  // )
+  // return React.createElement("h1", {}, "Hello from JS")
+
+  // const [count, setCount] = useState(0)
+
+  // return e("div", { className: "container" }, [
+  //   e("h1", {key: 1, className: "font-bold"}, `Test JSX ${count}`),
+  //   e("button", {
+  //     key: 2,
+  //     className: "py-2 px-4 border",
+  //     onClick: () => {
+  //       setCount(count + 1)
+  //       // console.log(count)
+  //     }
+  //   }, "Click me!")
+  // ])
 }
 
 export default App;
---
+++ src/components/Product.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react"
+import { IProduct } from "../models"
+
+interface ProductProps {
+    product: IProduct
+}
+
+export function Product({ product }: ProductProps) {
+    const [details, setDetails] = useState(false)
+
+    const btnBgClassName = details ? "bg-yellow-400" : "bg-blue-400"
+    const btnClasses = ["py-2 px-4 border", btnBgClassName]
+
+    return (
+        <div
+            className="border py-2 px-4 rounded flex flex-col items-center mb-2"
+        >
+            <img src={ product.image } className="w-1/6" alt={ product.title } />
+            <p>{ product.title }</p>
+            <span className="font-bold">{product.price}</span>
+            {/* <p>{ product.description }</p> */}
+            <button
+                // className="py-2 px-4 border bg-yellow-400"
+                className={btnClasses.join(" ")}
+                // onClick={() => setDetails(!details)}
+                onClick={() => setDetails(prev => !prev)}
+            >
+                <p>{ details ? "Hide" : "Show" } Details</p>
+            </button>
+
+            {details && <div>
+                <p>{ product.description }</p>
+                <p>Rate: <span style={{ fontWeight: "bold" }}>{product.rating.rate}</span></p>
+            </div>}
+        </div>
+    )
+}
