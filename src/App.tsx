8ea41bbe5919ad473f3d696960303521efabf016
Ivan Nikolaev <nikolaev_ia@nlmk.com>
2023-01-25 23:41:11 +0300

    вынесение функций модального окна во внешний контекст

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, {/* createElement as e, useEffect, */ useState} from 'react';
+import React,
+{ useState, useContext /* , createElement as e, useEffect */ } from 'react';
 
 // import { products } from "./data/products"
 
@@ -8,15 +9,19 @@ import { ErrorMessage } from "./components/ErrorMessage"
 import { Modal } from "./components/Modal"
 import { CreateProduct } from "./components/CreateProduct"
 import { IProduct } from "./models"
+import { ModalContext } from "./context/ModalContext"
 // import axios, { AxiosError } from "axios"
 
 import { useProducts } from "./hooks/products"
 
 function App() {
   const { loading, products, error, addProduct } = useProducts()
-  const [modal, setModal] = useState(false)
+  // const [modal, setModal] = useState(false)
+  const {modal, open: openModal, close: closeModal } = useContext(ModalContext)
+
+
   const createHandler = (product: IProduct) => {
-    setModal(false)
+    closeModal()
     // product.rating = product.rating || { rate: 0, count: 0 }
     addProduct(product)
   }
@@ -31,13 +36,13 @@ function App() {
       {/* <Product product={ products[0] } /> */}
       {/* <Product product={ products[1] } /> */}
 
-      {modal && <Modal title="Create new product" onClose={() => setModal(false)}>
+      {modal && <Modal title="Create new product" onClose={() => closeModal() }>
         <CreateProduct onCreate={ createHandler }/>
       </Modal>}
 
       <button
         className="fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2"
-        onClick={() => setModal(true)}
+        onClick={() => openModal()}
         >+</button>
     </div>
   )
