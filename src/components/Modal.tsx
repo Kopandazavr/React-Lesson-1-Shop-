c451df06a98dd01967ea39f17a15ded7059e01c5
Ivan Nikolaev <nikolaev_ia@nlmk.com>
2023-01-26 03:41:14 +0300

    123

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 
-interface ModalProps {
+interface IModalProps {
     children: React.ReactNode
     title: string
     onClose: () => void
 }
 
-export function Modal({ children, title, onClose }: ModalProps ) {
+export function Modal({ children, title, onClose }: IModalProps ) {
     return (
         <>
         <div className="fixed bg-black/50 top-0 left-0 right-0 bottom-0" onClick={ onClose } />
